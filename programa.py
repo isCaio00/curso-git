77ef8ec861dcdf0dfa195f74f7540fb73fabcefe
Caio Code <caiodoprado.p@gmail.com>
2026-09-12 22:56:37 -0300

    Atualiza a mensagem de aprendizado

--- programa.py
+++ programa.py
@@ -1,5 +1,5 @@
 nome = input("Digite seu nome: ")
 
 print(f"Olá, {nome}!")
-print("Estou aprendendo Git e Github.")
+print("Estou aprendendo Git profissionalmente.")
 print("Esta alteração foi feita em uma branch.")
