f61c04499daff100d38bc22bddf2433eb6f2db1e
Caio Code <caiodoprado.p@gmail.com>
2026-09-12 22:48:23 -0300

    atualização-programa-py

--- programa.py
+++ programa.py
@@ -1,5 +1,5 @@
 nome = input("Digite seu nome: ")
 
-print(f"Olá, {nome}!")
+print(f"Olá, {nome}!!")
 print("Estou aprendendo Git e Github.")
-print("Esta alteração foi feita em uma branch.")
+print("Estou desenvolvendo este projeto durante meus estudos de engenharia de software.") 
